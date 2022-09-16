955dfef81bfd9c5ebe27c6cdabcc96c42066892b
Sergey Kapralov <ser-kapralov@yandex.ru>
2022-09-16 15:30:57 +0300

    WIP

--- internal/hackers/repository/redis/redis.go
+++ internal/hackers/repository/redis/redis.go
@@ -1,6 +1,9 @@
 package redis
 
-import "github.com/go-redis/redis/v9"
+import (
+	"github.com/go-redis/redis/v9"
+	"github.com/kapralovs/hackers-list/internal/models"
+)
 
 type config struct {
 	client *redis.Client
@@ -16,9 +19,8 @@ func NewConfig(client *redis.Client) *config {
 	}
 }
 
-return &config{
-	client:redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	}),
+func GetHackersList(rc *redis.Client) ([]*models.Hacker, error) {
+	hackers := []*models.Hacker{}
+	// rc.ZRange(context.Background(),"hackers")
+	return hackers, nil
+}
