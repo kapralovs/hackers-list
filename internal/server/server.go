3fc55d54648a6f8f72d15b1490c0301242ea9d4f
kapralovs <ser-kapralov@yandex.ru>
2022-09-18 00:41:21 +0300

    Fix

--- internal/server/server.go
+++ internal/server/server.go
@@ -23,7 +23,7 @@ func New(port string, router *fiber.App) *server {
 
 func (s *server) Run() error {
 	redisClient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
